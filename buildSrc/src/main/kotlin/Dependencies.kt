13ad05cdeeee18fa7efc6a24b94d4d95244b006e
Prempal Singh <prempal.42@gmail.com>
2021-05-13 00:26:45 +0530

    1/n migrate to kotlin dsl

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,6 @@
+object deps {
+    object versions {
+        const val kotlin = "1.4.21"
+        const val hilt = "2.35"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "TestHilt"
+
+include(":homedemo")
+include(":detaildemo")
+include(":lib")
+include(":detail")
+include(":home")
+include(":app")
